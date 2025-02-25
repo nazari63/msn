fd0f768358ba43f1407ad696252521f21d42cdbb
msn2022 <93134182+nazari63@users.noreply.github.com>
2025-02-25 17:51:50 +0100

    Update msn.sol

--- msn.sol
+++ msn.sol
@@ -14,13 +14,13 @@ interface IERC20 {
 }
 
 contract MSNToken is IERC20 {
-    uint public totalSupply;
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
+    uint public override totalSupply;
+    mapping(address => uint) public override balanceOf;
+    mapping(address => mapping(address => uint)) public override allowance;
     
-    string public name = "msn";
-    string public symbol = "MSN";
-    uint8 public decimals = 18;
+    string public override name = "msn";
+    string public override symbol = "MSN";
+    uint8 public override decimals = 18;
 
     constructor() {
         totalSupply = 1_000_000_000 * 10 ** uint(decimals); // 1 میلیارد توکن
@@ -28,39 +28,52 @@ contract MSNToken is IERC20 {
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function transfer(address recipient, uint amount) external returns (bool) {
+    // Transfer function with Checks-Effects-Interactions pattern
+    function transfer(address recipient, uint amount) public override returns (bool) {
         require(amount <= balanceOf[msg.sender], "Insufficient balance");
+        require(!isContract(recipient), "Cannot send tokens to a contract");
+
+        // Checks-Effects-Interactions pattern
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
+        
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function approve(address spender, uint amount) external returns (bool) {
+    function approve(address spender, uint amount) public override returns (bool) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint amount) external returns (bool) {
+    function transferFrom(address sender, address recipient, uint amount) public override returns (bool) {
         require(amount <= balanceOf[sender], "Insufficient balance");
         require(amount <= allowance[sender][msg.sender], "Allowance exceeded");
+        require(!isContract(recipient), "Cannot send tokens to a contract");
 
+        // Checks-Effects-Interactions pattern
         allowance[sender][msg.sender] -= amount;
+        
+        if (allowance[sender][msg.sender] == 0) {
+            allowance[sender][msg.sender] = 0; // Optional but for clarity
+        }
+        
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
+        
         emit Transfer(sender, recipient, amount);
         return true;
     }
 
     // Functions to increase or decrease allowance
-    function increaseAllowance(address spender, uint addedValue) public returns (bool) {
+    function increaseAllowance(address spender, uint addedValue) external returns (bool) {
         allowance[msg.sender][spender] += addedValue;
         emit Approval(msg.sender, spender, allowance[msg.sender][spender]);
         return true;
     }
 
-    function decreaseAllowance(address spender, uint subtractedValue) public returns (bool) {
+    function decreaseAllowance(address spender, uint subtractedValue) external returns (bool) {
         require(subtractedValue <= allowance[msg.sender][spender], "Decreased allowance below zero");
         allowance[msg.sender][spender] -= subtractedValue;
         emit Approval(msg.sender, spender, allowance[msg.sender][spender]);
